952a0cbee5145419d559293a5e41759a5c32cfea
F. Sweijen <sweijen@steve.intra.astro.rug.nl>
2015-05-07 14:16:14 +0200

    Initial commit

---
+++ groningen_weather.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+from __future__ import division
+from matplotlib.pyplot import figure, show
+
+import math
+import numpy as np
+
+DATE_M, HOUR_M, TEMP_M, CLOUD_M = [], [], [], []
+YR_START = 0; YR_END = 0
+###########################
+# Pre-use data reduction. #
+###########################
+def reduce_data():
+    ''' Reduce the KNMI data to a workable format for further analysis. It extracts the hour, temperature and cloud coverage accessible as data[year][month][day][hour].
+    It modifies global variables defined at the start of the script.
+    Args:
+        None
+    Returns:
+        None
+    '''
+    print 'Processing hourly data.'
+    date, hour, temp, cloud = np.genfromtxt('./KNMI_20150424_hourly.txt', delimiter=',', filling_values=None, skip_header=14, usecols=(1,2,3,4), unpack=True)
+    # Convert temperatures to celcius.
+    temp /= 10
+    print '[Hourly] Grouping data by hour...'
+
+    # Group the data per year: date = [year1, year2, ...] > year = [hour1, hour2, ...]
+    print '[Hourly] Grouping data by year...'
+    date_y, hours_y, temp_y, cloud_y = [], [], [], []
+    dy, hy, ty, cy = [], [], [], []
+    nyear = date[0]//1e4 + 1
+    for k, h in enumerate(date): # For every hour...
+        year = int(h//1e4)
+        if year < nyear:
+            dy.append(h); hy.append(hour[k]); ty.append(temp[k]); cy.append(cloud[k])
+        else:
+            date_y.append(dy); hours_y.append(hy); temp_y.append(ty); cloud_y.append(cy)
+            nyear += 1
+            dy, hy, ty, cy = [], [], [], []
+            dy.append(h); hy.append(hour[k]); ty.append(temp[k]); cy.append(cloud[k])
+    # Append the remaining, incomplete year.
+    date_y.append(dy); hours_y.append(hy); temp_y.append(ty); cloud_y.append(cy)
+
+    # Group the data by hour: data = [year1, year2, ...] > year = [day1, day2, ...] > day = [hour1, hour2, ...]
+    print '[Hourly] Grouping data by hour...'
+    date_h, hour_h, temp_h, cloud_h = [], [], [] ,[]
+    for k, yr in enumerate(date_y):
+        dh, hh, th, ch = [], [], [] ,[]
+        for i in range(len(yr)//24):
+            dh.append(date_y[k][24*i])
+            hh.append(hours_y[k][24*i:24*(i+1)])
+            th.append(temp_y[k][24*i:24*(i+1)])
+            ch.append(cloud_y[k][24*i:24*(i+1)])
+        date_h.append(dh); hour_h.append(hh); temp_h.append(th); cloud_h.append(ch)
+
+    # Group the data by month: data = [year1, year2, ...] > year = [month1, month2, ...] > month = [day1, day2, ...] > day = [hour1, hour2, ...]
+    print '[Hourly] Grouping data by month...'
+    date_m, hour_m, temp_m, cloud_m = [], [] ,[], []
+    for i, yr in enumerate(date_h):
+        dm, hm, tm, cm = [], [], [] ,[]
+        for _ in range(12):
+            dm.append([])
+            hm.append([])
+            tm.append([])
+            cm.append([])
+        for d, h, t, c in zip(date_h[i], hour_h[i], temp_h[i], cloud_h[i]):
+            month = int((d % 1e4) // 100)
+            dm[month-1].append(int(d))
+            hm[month-1].append(h)
+            tm[month-1].append(t)
+            cm[month-1].append(c)
+        if [] in dm: # There is an incomplete year, add all available months.
+            date_m.append(dm[:dm.index([])])
+            hour_m.append(hm[:hm.index([])])
+            temp_m.append(tm[:tm.index([])])
+            cloud_m.append(cm[:cm.index([])])
+        else:
+            date_m.append(dm)
+            hour_m.append(hm)
+            temp_m.append(tm)
+            cloud_m.append(cm)
+    global DATE_M, HOUR_M, TEMP_M, CLOUD_M
+    global YR_START, YR_END
+    DATE_M = date_m; HOUR_M = hour_m; TEMP_M = temp_m; CLOUD_M = cloud_m
+    YR_START = int(date[0] // 1e4); YR_END = int(date[len(date)-1] //1e4)
+
+def monDayNightAvg(data, yyyy, mm=-1, yr_start=2010, yr_end=2015):
+    ''' Calculate monthly average for the day hours and for the night hours separately.
+    Gives the monthly average for the specified month. If month is set to -1 it will iterate over the given year and calculate the average for all months available.
+    Args:
+        ndarray data - array containing data to analyze. Should be accessible as data[year][month][day][hour]
+        float yyyy - the year in yyyy format.
+        float mm - the month in mm format, normally assumes all months.
+    Returns:
+        ndarray/float nam - nightly average and standard deviation, 00-06.
+        ndarray/float mam - morning average and standard deviation, 06-12.
+        ndarray/float aam - afternoon average and standard deviation, 12-18.
+        ndarray/float eam - evening average and standard deviation, 18-24
+    '''
+    if mm < 0: # User wants all months.
+        nam, mam, aam, eam = [], [], [], []
+        nams, mams, aams, eams = [], [], [], []
+        for month in data[int(yr_end-yyyy)]:
+            nad, mad, aad, ead = [], [], [], []
+            for day in month:
+                na = np.nanmean(day[0:6]); nad.append(na)
+                ma = np.nanmean(day[6:12]); mad.append(ma)
+                aa = np.nanmean(day[12:18]); aad.append(aa)
+                ea = np.nanmean(day[18:24]); ead.append(ea)
+            nam.append(np.nanmean(nad))
+            mam.append(np.nanmean(mad))
+            aam.append(np.nanmean(aad))
+            eam.append(np.nanmean(ead))
+        return np.asarray(nam), np.asarray(mam), np.asarray(aam), np.asarray(eam)
+    else: # User specifies a month.
+        # Select the required month.
+        dat = data[int(yr_end - yyyy)][mm]
+        nad, mad, aad, ead = [], [], [], []
+        for day in dat:
+            na = np.nanmean(day[0:6]); nad.append(na)
+            ma = np.nanmean(day[6:12]); mad.append(ma)
+            aa = np.nanmean(day[12:18]); aad.append(aa)
+            ea = np.nanmean(day[18:24]); ead.append(ea)
+        return np.asarray(nad), np.asarray(mad), np.asarray(aad), np.asarray(ead)
+        
+reduce_data()
+##############################################################################################################################################################
+# Data processing finished. Access the data as date_m[year][month][day] or variable[year][month][day][hour] where variable can be hour_m, temp_m or cloud_m. #
+##############################################################################################################################################################
+if __name__ == '__main__':
+    # Demonstration of hourly results.
+    DATE = 2013021518
+    #DATE = input('Enter date as yyyymmdd: ')
+    yyyy = int(DATE // 1e6)
+    mm = int((DATE % 1e6) // 1e4)
+    dd = int((DATE % 1e4) // 100)
+    indx_y = yyyy - YR_START
+    indx_m = mm - 1
+    indx_d = dd - 1
+
+    print '[Hourly] Plotting results...'
+    hrs = HOUR_M[indx_y][indx_m][indx_d]
+    tem = TEMP_M[indx_y][indx_m][indx_d]
+    cls = CLOUD_M[indx_y][indx_m][indx_d]
+    fig = figure('Hourly Temperature')
+    ax = fig.add_subplot(111)
+    ax2 = ax.twinx(); ax2.set_ylim(-1, 10)
+    ax.plot(hrs, tem, 'ro-', label='Temperature')
+    ax2.plot(hrs, cls, 'bo', label='Cloudiness')
+    ax.set_title('Hourly Temperature for %.4d-%.2d-%.2d' % (yyyy, mm, dd), fontweight='bold')
+    ax.set_xlabel('Time [hr]'); ax.set_ylabel('Temperature [$\\degree$C]')
+    ax2.set_ylabel('Cloudiness [octants]')
+    ax.set_xticks(range(25))
+    ax.legend(loc=(0.65,0.15)); ax2.legend(loc=(0.65, 0.05))
+    labels = [str(x) for x in range(25)]; labels[0] = ''
+    ax.set_xticklabels(labels)
+    #show()
+
+    # Demonstration of separate averages per part of the day.
+    yr = 2014; mnth = -1
+    n, m, a, e = monDayNightAvg(CLOUD_M, yr, mm=mnth)
+    fig = figure('Separated Monthly Cloud Coverage')
+    ax = fig.add_subplot(411)
+    ax2 = fig.add_subplot(412)
+    ax3 = fig.add_subplot(413)
+    ax4 = fig.add_subplot(414)
+    ax.set_title('Average Cloud Coverage by Part of the Day for %.4d-%.2d\nNight' % (yr, mnth), fontweight='bold')
+    ax.set_xlim(-1, 32); ax.set_ylim(-1, 9)
+    ax.get_xaxis().set_visible(False)
+    ax.plot(range(len(n)), n, 'go-', label='Night')
+    ax2.set_title('Morning', fontweight='bold')
+    ax2.plot(range(len(n)), m, 'bo-', label='Morning')
+    ax2.set_xlim(-1, 32); ax2.set_ylim(-1, 9)
+    ax2.get_xaxis().set_visible(False)
+    ax3.set_title('Afternoon', fontweight='bold')
+    ax3.plot(range(len(n)), a, 'ro-', label='Afternoon')
+    ax3.set_xlim(-1, 32); ax3.set_ylim(-1, 9)
+    ax3.get_xaxis().set_visible(False)
+    ax4.set_title('Evening', fontweight='bold')
+    ax4.plot(range(len(n)), e, 'co-', label='Evening')
+    ax4.xaxis.set_ticks(np.linspace(ax.get_xlim()[0], ax.get_xlim()[1], len(n)+3))
+    ax4.set_xlim(-1, 32); ax4.set_ylim(-1, 9)
+    ax4.set_xlabel('Day'); ax4.set_ylabel('Cloud Coverage\n[octants]')
+    show()
